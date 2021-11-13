c58e1ddcfbddf58ac50785bc08bc09b10797c90e
MaksymilianK <kondziugeniusz@gmail.com>
2021-11-13 01:28:08 +0100

    Init project

---
+++ src/main/java/com/github/maksymiliank/arrivalbouncer/ArrivalBouncer.java
@@ -0,0 +1,11 @@
+package com.github.maksymiliank.arrivalbouncer;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class ArrivalBouncer extends Plugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
